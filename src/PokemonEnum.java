e91414a34ffd4bf0bcc805e31e30fb0eeb76d8af
Jann <162745758+JannC23@users.noreply.github.com>
2025-05-15 13:40:01 -0500

    Update PokemonEnum.java

--- src/PokemonEnum.java
+++ src/PokemonEnum.java
@@ -1,5 +1,4 @@
 public enum PokemonEnum {
-<<<<<<< HEAD
     CHARMANDER(1, "Charmander", Pokemon.Tipo.FUEGO, 180, 52, 43, 60, 50, 65, "charmander.gif"),
     BULBASAUR(2, "Bulbasaur", Pokemon.Tipo.PLANTA, 200, 49, 49, 65, 65, 45, "bulbasaur.gif"),
     PIKACHU(3, "Pikachu", Pokemon.Tipo.ELECTRICO, 150, 55, 40, 50, 50, 90, "pikachu.gif"),
@@ -9,12 +8,6 @@ public enum PokemonEnum {
     FLAREON(7, "Flareon", Pokemon.Tipo.FUEGO, 230, 130, 60, 95, 110, 65, "flareon.gif"),
     VAPOREON(8, "Vaporeon", Pokemon.Tipo.AGUA, 260, 65, 60, 110, 95, 65, "vaporeon.gif"),
     GROWLITHE(9, "Growlithe", Pokemon.Tipo.FUEGO, 190, 70, 45, 70, 50, 60, "growlithe.gif");
-=======
-    CHARMANDER(1, "Charmander", Pokemon.Tipo.FUEGO, 180, 52, 43, 60, 50, 65),
-    BULBASAUR(2, "Bulbasaur", Pokemon.Tipo.PLANTA, 200, 49, 49, 65, 65, 45),
-    PIKACHU(3, "Pikachu", Pokemon.Tipo.ELECTRICO, 150, 55, 40, 50, 50, 90),
-    SQUIRTLE(4, "Squirtle", Pokemon.Tipo.AGUA, 220, 48, 65, 50, 64, 43);
->>>>>>> origin/main
 
     private final int numero;
     private final String nombre;
@@ -25,14 +18,9 @@ public enum PokemonEnum {
     private final int ataqueEspecial;
     private final int defensaEspecial;
     private final int velocidad;
-<<<<<<< HEAD
     private final String gifPath;
 
     PokemonEnum(int numero, String nombre, Pokemon.Tipo tipo, int puntosSalud, int ataque, int defensa, int ataqueEspecial, int defensaEspecial, int velocidad, String gifPath) {
-=======
-
-    PokemonEnum(int numero, String nombre, Pokemon.Tipo tipo, int puntosSalud, int ataque, int defensa, int ataqueEspecial, int defensaEspecial, int velocidad) {
->>>>>>> origin/main
         this.numero = numero;
         this.nombre = nombre;
         this.tipo = tipo;
@@ -42,10 +30,7 @@ public enum PokemonEnum {
         this.ataqueEspecial = ataqueEspecial;
         this.defensaEspecial = defensaEspecial;
         this.velocidad = velocidad;
-<<<<<<< HEAD
         this.gifPath = gifPath;
-=======
->>>>>>> origin/main
     }
 
     public int getNumero() {
@@ -83,11 +68,8 @@ public enum PokemonEnum {
     public int getVelocidad() {
         return velocidad;
     }
-<<<<<<< HEAD
 
     public String getGifPath() {
         return gifPath;
     }
-=======
->>>>>>> origin/main
 }
