7c3f1a0380443c36c4da78da2173e4ead0176e14
Jann <162745758+JannC23@users.noreply.github.com>
2025-05-15 13:36:17 -0500

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,13 +1,6 @@
-<<<<<<< HEAD
 // Clase principal que inicia la interfaz gráfica del programa
 public class App {
     public static void main(String[] args) {
         new Interfaz(); // Crea una nueva instancia de la interfaz gráfica
-=======
-public class App {
-    public static void main(String[] args) {
-        MenuPrincipal menu = new MenuPrincipal();
-        menu.iniciarMenu(); // Llamar al método para iniciar el menú
->>>>>>> origin/main
     }
 }
