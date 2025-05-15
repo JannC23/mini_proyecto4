5f08b5fc2c0746c10bcd87d37938f35a7f874f0e
Jann <162745758+JannC23@users.noreply.github.com>
2025-05-15 13:37:56 -0500

    Update Entrenador.java

--- src/Entrenador.java
+++ src/Entrenador.java
@@ -1,5 +1,4 @@
 import java.util.ArrayList;
-<<<<<<< HEAD
 
 // Clase que representa un entrenador Pokémon
 // Cada entrenador tiene un nombre y un equipo de Pokémon
@@ -10,17 +9,6 @@ public class Entrenador {
 
     public Entrenador(String nombre) {
         this.nombre = nombre;
-=======
-import java.util.Scanner;
-
-public class Entrenador {
-    private String nombre;
-    private ArrayList<Pokemon> equipo = new ArrayList<>();
-
-    public Entrenador(String nombre) {
-        this.nombre = nombre;
-        this.equipo = new ArrayList<>();
->>>>>>> origin/main
     }
 
     public String getNombre() {
@@ -36,7 +24,6 @@ public class Entrenador {
     }
 
     public void setEquipo(ArrayList<Pokemon> equipo) {
-<<<<<<< HEAD
         this.equipo = equipo;
     }
 
@@ -72,33 +59,5 @@ public class Entrenador {
             }
         }
         return true;
-=======
-        if (equipo.size() > 3) {
-            System.out.println("El equipo no puede tener más de tres Pokémon");
-            return;
-        }
-        this.equipo.clear();
-        for (Pokemon pokemon : equipo) {
-            this.equipo.add(pokemon);
-        }
-    }
-
-    public Pokemon elegirPokemonParaBatalla() {
-        Scanner entrada = new Scanner(System.in);
-        System.out.println("\n" + nombre + ", selecciona un Pokémon para la batalla:");
-        for (int i = 0; i < equipo.size(); i++) {
-            Pokemon pokemon = equipo.get(i);
-            System.out.println((i + 1) + ". " + pokemon.getNombre() + " (HP: " + pokemon.getPuntosSalud() + ")");
-        }
-        int opcion;
-        do {
-            System.out.print("Seleccione el número del Pokémon: ");
-            opcion = entrada.nextInt();
-            if (opcion < 1 || opcion > equipo.size()) {
-                System.out.println("Opción no válida. Intente nuevamente.");
-            }
-        } while (opcion < 1 || opcion > equipo.size());
-        return equipo.get(opcion - 1);
->>>>>>> origin/main
     }
 }
