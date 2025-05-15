f294a3abb0e4d28b61d1d7de3f4643feac7b55cb
Jann <162745758+JannC23@users.noreply.github.com>
2025-05-15 13:37:15 -0500

    Update Ataque.java

--- src/Ataque.java
+++ src/Ataque.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // Clase que representa un ataque que puede realizar un Pokémon
 public class Ataque {
     public enum TipoDanio { FISICO, ESPECIAL } // Tipos de daño que puede causar un ataque
@@ -7,49 +6,14 @@ public class Ataque {
     private int potencia; // Potencia del ataque
 
     // Constructor para inicializar un ataque con su nombre, tipo de daño y potencia
-=======
-public class Ataque {
-    private String nombre;
-    private String tipoDano;
-    private double potencia;
-
->>>>>>> origin/main
     public Ataque(String nombre, String tipoDano, int potencia) {
         this.nombre = nombre;
         this.tipoDano = tipoDano;
         this.potencia = potencia;
     }
 
-<<<<<<< HEAD
     // Métodos para obtener los atributos del ataque
     public String getNombre() { return nombre; }
     public String getTipoDano() { return tipoDano; }
     public int getPotencia() { return potencia; }
-=======
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getTipoDano() {
-        return tipoDano;
-    }
-
-    public void setTipoDano(String tipoDano) {
-        this.tipoDano = tipoDano;
-    }
-
-    public double getPotencia() {
-        return potencia;
-    }
-
-    public void setPotencia(int potencia) {
-        this.potencia = potencia;
-    }
-    
-    
->>>>>>> origin/main
 }
